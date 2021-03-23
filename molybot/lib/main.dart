659d9aab1d9cc0d014c3b574e35c712c299bc53e
Arjin Renas Akgül <aakgul16@ku.edu.tr>
2021-03-23 21:33:32 +0300

    updated

--- molybot/lib/main.dart
+++ molybot/lib/main.dart
@@ -1,66 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:molybot/screens/profile_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import './screens/main_screen.dart';
 import './screens/coin_screen.dart';
 import './providers/trades.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async{
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 } 
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return MultiProvider(
-    //   providers: [
-    //     ChangeNotifierProvider.value(value: Trades())
-    //   ],
-    //   child: MaterialApp(
-    //     title: 'Molybot',
-        // routes: 
-        // {
-        //   Coin.routeName: (ctx) => Coin(),
-        // },
-    //     home: MainScreen(),
-        
-    //   ) ,);
-    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, appSnapshot) {
-
-
-      return MultiProvider(
-        providers: [
+    return MultiProvider(
+      providers: [
         ChangeNotifierProvider.value(value: Trades())
-        ],
-          child: MaterialApp(
-            routes: 
-          {
+      ],
+      child: MaterialApp(
+        title: 'Molybot',
+        home: MainScreen(),
+        routes: 
+        {
           Coin.routeName: (ctx) => Coin(),
-          },
-          title: 'Molybot',
-          
-          home: appSnapshot.connectionState != ConnectionState.done ? Center(
-                  child: CircularProgressIndicator()) : StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (ctx, userSnapshot) {
-            if (userSnapshot.connectionState == ConnectionState.waiting) {
-              return Text("Waiting");
-            }
-            if (userSnapshot.hasData) {
-              return MainScreen();
-            }
-            return ProfileScreen();
-          }),
-        ),
-      );
-
-    });
+        },
+      ) ,)
+      ;
   }
 }
 
