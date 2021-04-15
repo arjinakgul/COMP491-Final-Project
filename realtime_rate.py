ef98b30ed320a289a146622f91c1b77630697b92
Arjin Renas Akgül <aakgul16@ku.edu.tr>
2021-04-15 21:28:16 +0300

    updated

--- realtime_rate.py
+++ realtime_rate.py
@@ -1,13 +1,13 @@
 
 from binance.websockets import BinanceSocketManager
 import pprint
-from firebase import firebase
+#from firebase import firebase
 import time
 from binance.client import Client
 
 
 
-firebase = firebase.FirebaseApplication("enter your firebase url", None)
+#firebase = firebase.FirebaseApplication("enter your firebase url", None)
 key = "enter your Binance API Key"
 secret = "enter your Binance secret key"
 
@@ -19,25 +19,25 @@ info = client.get_exchange_info()
 bm = BinanceSocketManager(client)
 
 def process_messageETH(msg):
-    noweth=round(time.time() * 1000)
-    firebase.post("/rates/ETHUSDT", (msg["k"]["c"],msg["E"]))
-    db=firebase.get("/rates/ETHUSDT",None)
-    k=list(db.keys())[0]
-    a=db[k]
-    if noweth-a[1]>3600000:
-        firebase.delete("/rates/ETHUSDT",k)
-
+    #noweth=round(time.time() * 1000)
+    #firebase.post("/rates/ETHUSDT", (msg["k"]["c"],msg["E"]))
+    #db=firebase.get("/rates/ETHUSDT",None)
+    #k=list(db.keys())[0]
+    #a=db[k]
+    #if noweth-a[1]>3600000:
+     #   firebase.delete("/rates/ETHUSDT",k)
+    pprint.pprint(msg)
 
 def process_messageBTC(msg):
 	
-    nowbtc=round(time.time() * 1000)
-    firebase.post("/rates/BTCUSDT", (msg["k"]["c"],msg["E"]))
-    db=firebase.get("/rates/BTCUSDT",None)
-    k=list(db.keys())[0]
-    a=db[k]
-    if nowbtc-a[1]>3600000:
-        firebase.delete("/rates/BTCUSDT",k)
-    
+#    nowbtc=round(time.time() * 1000)
+ #   firebase.post("/rates/BTCUSDT", (msg["k"]["c"],msg["E"]))
+  #  db=firebase.get("/rates/BTCUSDT",None)
+   # k=list(db.keys())[0]
+    #a=db[k]
+    #if nowbtc-a[1]>3600000:
+     #   firebase.delete("/rates/BTCUSDT",k)
+    pprint.pprint(msg)
 	
 
 
